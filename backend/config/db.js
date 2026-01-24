3d938419e137c96d376dd597453f924ee125a182
AryanRacha <aryanracha@gmail.com>
2026-01-24 14:33:21 +0530

    Added start script

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            // useNewUrlParser: true, // Deprecated in newer mongoose, but good for older versions
-            // useUnifiedTopology: true,
-        });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
